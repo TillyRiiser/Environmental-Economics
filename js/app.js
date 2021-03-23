49c4c1dac015461329e8043cee6b003f3cd42f16
TillyRiiser <tillyriiser@gmail.com>
2021-03-23 12:15:46 +0000

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var donutCities = {
+    cities: ['Amsterdam','Copenhagen'],
+    title: 'Donut Cities',
+    number: 9999,
+    quote: function() {
+        console.log('Donut donut donut');
+    },
+    addCity: function(city) {
+        this.cities.push(city);
+    },
+    city: 'bath',
+}
